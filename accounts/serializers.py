627aa8a9b9c0ff736281cf883af976a587e13184
J Andrew Jefferson <140247568+Drew-Jeff@users.noreply.github.com>
2025-04-25 18:56:24 +0530

    Update serializers.py

---
+++ accounts/serializers.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User  # Import User model
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+
+    def create(self, validated_data):
+        """Create a new user with hashed password."""
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+        user.set_password(validated_data['password'])  # Hash password
+        user.save()
+        return user
